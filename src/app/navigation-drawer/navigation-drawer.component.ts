c4f770ff784a17f28e043937b2f714b944bda9c9
xaraso <121126841+xaraso@users.noreply.github.com>
2023-03-06 16:48:58 +0100

    5 files modified

--- src/app/navigation-drawer/navigation-drawer.component.ts
+++ src/app/navigation-drawer/navigation-drawer.component.ts
@@ -4,25 +4,30 @@ import { IgxNavigationDrawerComponent } from 'igniteui-angular';
 @Component({
   selector: 'app-navigation-drawer',
   templateUrl: './navigation-drawer.component.html',
-  styleUrls: ['./navigation-drawer.component.scss']
+  styleUrls: ['./navigation-drawer.component.scss'],
 })
 export class NavigationDrawerComponent {
   public navItems = [
-    { name: 'account_circle', text: 'Avatar' },
-    { name: 'error', text: 'Badge' },
-    { name: 'group_work', text: 'Button Group' }
-];
+    { name: 'account_circle', text: 'Presencia en máquina' },
+    { name: 'error', text: 'Tareas' },
+    { name: 'group_work', text: 'Ordenes' },
+    { name: 'account_circle', text: 'Incidencias' },
+    { name: 'error', text: 'CheckList' },
+    { name: 'account_circle', text: 'Documentos' },
+    { name: 'error', text: 'Mensajería' },
+    { name: 'group_work', text: 'Datos Tubo' },
+  ];
 
-public selected = 'Avatar';
+  public selected = 'Avatar';
 
-public navigate(item) {
+  public navigate(item) {
     this.selected = item.text;
-}
+  }
 
-@ViewChild(IgxNavigationDrawerComponent, { static: false })
-public drawer: IgxNavigationDrawerComponent;
-@HostListener('window:keydown.esc', ['$event'])
-handleKeyDown(event: KeyboardEvent) {
-  this.drawer.toggle();
-}
+  @ViewChild(IgxNavigationDrawerComponent, { static: false })
+  public drawer: IgxNavigationDrawerComponent;
+  @HostListener('window:keydown.esc', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    this.drawer.toggle();
+  }
 }
