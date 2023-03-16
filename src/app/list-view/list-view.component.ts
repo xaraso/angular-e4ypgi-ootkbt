cf978aa550e019942c1840b3f71d61c509f49cba
xaraso <121126841+xaraso@users.noreply.github.com>
2023-03-16 16:58:23 +0100

    4 files modified, 1 added

--- src/app/list-view/list-view.component.ts
+++ src/app/list-view/list-view.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { DisplayDensity, IgxFilterOptions } from 'igniteui-angular';
+import {
+  DisplayDensity,
+  IgxColumnComponent,
+  IgxFilterOptions,
+} from 'igniteui-angular';
 import { DATA } from '../data/customers';
+import { COLUMNAS } from '../data/header';
 @Component({
   selector: 'app-list-view',
   templateUrl: './list-view.component.html',
@@ -8,8 +13,24 @@ import { DATA } from '../data/customers';
 })
 export class ListViewComponent {
   public data: any[];
+  public columnas: any[];
 
   constructor() {
     this.data = DATA;
+    this.columnas = COLUMNAS;
+  }
+  public columnInit(args: IgxColumnComponent) {
+    // args.autosize();
+    this.columnas.forEach((e: any) => {
+      if (args.field == e.Nombre) {
+        if (e.Ancho < 0) {
+          args.width = 'auto';
+        } else if (e.Ancho > 0) {
+          args.width = e.Ancho;
+        } else {
+          args.hidden = true;
+        }
+      }
+    });
   }
 }
