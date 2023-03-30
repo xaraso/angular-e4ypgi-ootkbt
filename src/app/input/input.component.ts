a46e21eefbcb60a23ab3796fa7e76b6ac4169392
xaraso <121126841+xaraso@users.noreply.github.com>
2023-03-30 20:42:20 +0200

    2 files modified

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+import { IGX_INPUT_GROUP_TYPE } from 'igniteui-angular';
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
+  providers: [{ provide: IGX_INPUT_GROUP_TYPE, useValue: 'box' }],
 })
-export class InputComponent {
+export class InputComponent implements AfterViewInit {
   prueba() {
     // console.log('a ', collection[0]);
 
@@ -13,5 +14,25 @@ export class InputComponent {
 
     const slide = slides[0] as HTMLElement;
     slide.style.height = '30px';
+  }
+
+  ngAfterViewInit() {
+    const val = 45;
+    if (val === null) {
+      return;
+    }
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach((i) => {
+      i.style.height = val.toString() + 'px';
+    });
+
+    const inputGroups = document.querySelectorAll(
+      'igx-input-group'
+    ) as NodeListOf<HTMLElement>;
+    inputGroups.forEach((i) => {
+      i.style.height = val.toString() + 'px';
+    });
+    const combo = document.querySelector('igx-combo') as HTMLElement;
+    combo.style.height = val.toString() + 'px';
   }
 }
